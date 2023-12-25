f57d85b3ef20d97f0a744a4378af7d1b96b40630
Ofir Kaspi <kaspiofir@gmail.com>
2023-12-25 20:20:38 +0200

    F/Styling/Added image for the campground model via unsplash random photo collection

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({})
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(descriptors)}`
+            title: `${sample(descriptors)} ${sample(descriptors)}`,
+            image: 'http://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, impedit officia culpa aliquam ducimus cum eum porro! Aliquam, praesentium? Voluptatem eaque fugit recusandae ipsa perspiciatis sit nesciunt, eligendi ea? Sit?',
+            price
         })
         await camp.save()
     }
