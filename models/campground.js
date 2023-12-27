0a2631dc1783dcb913912d2bdf312b440766c3a7
Ofir Kaspi <kaspiofir@gmail.com>
2023-12-27 22:15:20 +0200

    F/Review/Mongoose middleware for deleting reviews of deleted campground

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Review } from "./review.js";
 
 const Schema = mongoose.Schema
 
@@ -14,6 +15,17 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+})
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    console.log(doc);
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
 const Campground = mongoose.model('Campground', CampgroundSchema);
