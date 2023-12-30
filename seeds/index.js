fadfa5bf7fe32292c42eaf26926e63a005a22a3d
Ofir Kaspi <kaspiofir@gmail.com>
2023-12-30 22:36:38 +0200

    F/Authorazation/Added author to campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-import Campground from '../models/campground.js'
+import { Campground } from '../models/campground.js'
 import { cities } from './cities.js'
 import { descriptors, places } from './seedHelpers.js'
 
@@ -19,6 +19,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
+            author: '659063a36b6b6f80f6149e59',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(descriptors)}`,
             image: 'http://source.unsplash.com/collection/483251',
